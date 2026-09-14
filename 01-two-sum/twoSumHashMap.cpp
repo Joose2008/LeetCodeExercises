b84b12911f815058155431e32d48e4d09138b065
Joose2008 <jffernandezc@unimagdalena.edu.co>
2026-09-13 19:18:02 -0500

    feat: Creación de solución optima a twoSum

---
+++ 01-two-sum/twoSumHashMap.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+ public: 
+    vector<int> twoSum(std::vector<int>& nums, int target) {
+       unordered_map<int, int> num_map;
+
+       for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (num_map.contains(complement)) {
+                return {num_map[complement], i}; // Retorno índices
+            }
+            num_map[nums[i]] = i;
+        }
+        return {}; // Retorno vacío si no encuentra solución
+    }
+}; 
