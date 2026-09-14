4fdc9fdfd0402a02c92d0ac032f2cec51247c773
Joose2008 <jffernandezc@unimagdalena.edu.co>
2026-09-13 19:21:17 -0500

    fix: Código limpio twoSumHashMap.cpp

--- 01-two-sum/twoSumHashMap.cpp
+++ 01-two-sum/twoSumHashMap.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
  public: 
-    vector<int> twoSum(std::vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {
        unordered_map<int, int> num_map;
 
        for (int i = 0; i < nums.size(); i++) {
